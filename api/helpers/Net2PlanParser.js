049fc02c66a3d807d2b121b7193c5696524582d6
iicc1 <ignacio.iglesias@hotmail.es>
2020-04-22 20:13:22 +0200

    Added support for Net2Plan topologies

---
+++ api/helpers/Net2PlanParser.js
@@ -0,0 +1,49 @@
+const fs = require('fs').promises
+const xml2js = require('xml2js')
+
+const topologyParser = async (inputFile) => {
+  const topologyBuffer = await fs.readFile('topologies/Net2Plan/' + inputFile + '.n2p')
+
+  const topology = {
+    nodes: [],
+    links: []
+  }
+
+  await xml2js.parseStringPromise(topologyBuffer).then((result) => {
+    if (result.network.physicalTopology) {
+      for (const node of result.network.physicalTopology[0].node) {
+        topology.nodes.push({ id: node.$.id, name: node.$.name, latitude: node.$.yCoord, longitude: node.$.xCoord })
+      }
+      if (result.network.physicalTopology[0].layer) {
+        for (const link of result.network.physicalTopology[0].layer) {
+          topology.links.push({ id: link.$.id, originId: link.$.originNodeId, destinationId: link.$.destinationNodeId })
+        }
+      }
+    } else {
+      for (const node of result.network.node) {
+        topology.nodes.push({ id: node.$.id, name: node.$.name, latitude: node.$.yCoord, longitude: node.$.xCoord })
+      }
+      if (result.network.layer) {
+        for (const link of result.network.layer[0].link) {
+          topology.links.push({ id: link.$.id, originId: link.$.originNodeId, destinationId: link.$.destinationNodeId })
+        }
+      }
+    }
+  })
+    .catch((error) => {
+      console.log(error)
+    })
+  return {
+    topology: topology,
+    nodeNumber: topology.nodes.length,
+    linkNumber: topology.links.length
+  }
+}
+
+if (process.argv[2]) {
+  topologyParser(process.argv[2])
+}
+
+module.exports = {
+  topologyParser
+}
